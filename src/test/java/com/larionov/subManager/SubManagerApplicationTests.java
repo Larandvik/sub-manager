683500e0d1e53d9b6a4f34053a1c8fd7e1af27ff
Larandvik <0658203@gmail.com>
2025-05-06 13:59:17 -0300

    Initial commit

---
+++ src/test/java/com/larionov/subManager/SubManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.larionov.subManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
