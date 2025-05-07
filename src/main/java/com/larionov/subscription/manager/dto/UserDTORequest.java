6e6eb7d012149cd2905802fce076e967a6b0a745
Larandvik <0658203@gmail.com>
2025-05-07 17:32:44 -0300

    Add validation annotations to class UserDTORequest

--- src/main/java/com/larionov/subscription/manager/dto/UserDTORequest.java
+++ src/main/java/com/larionov/subscription/manager/dto/UserDTORequest.java
@@ -1,11 +1,17 @@
 package com.larionov.subscription.manager.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 
 @Builder
 public record UserDTORequest(
 
+        @NotBlank(message = "Name must not be empty")
         String name,
+
+        @NotBlank(message = "Email must not be empty")
+        @Email(message = "Email must be correct")
         String email
 
 ) {
